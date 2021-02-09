1a2022221823391dad8c57745c1e575b3017390b
Poncho <jplyles1212@gmail.com>
2021-02-08 20:09:12 -0800

    First commit

---
+++ Number_Wizard_Console/Number Wizard Console/Assets/NumberWizard.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Welcome to Number Wizard!");
+        Debug.Log("Pick a number, don't tell me what it is...");
+        Debug.Log("Highest number you can pick is 1000");
+        Debug.Log("Lowest number you can pick is 1"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
